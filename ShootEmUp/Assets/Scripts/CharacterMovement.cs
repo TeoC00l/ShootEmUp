2a1db89b8444b776f1322b7e54e8f02b135f5629
Teodor Tysklind <37851342+TeoC00l@users.noreply.github.com>
2020-09-08 12:07:38 +0200

    Update CharacterMovement.cs

--- ShootEmUp/Assets/Scripts/CharacterMovement.cs
+++ ShootEmUp/Assets/Scripts/CharacterMovement.cs
@@ -1,6 +1,6 @@
 ﻿//@Author: Teodor Tysklind / FutureGames / Teodor.Tysklind@FutureGames.nu
 
-using System.Transactions;
+using System;
 using UnityEngine;
 
 namespace ShootEmUp
@@ -8,12 +8,18 @@ namespace ShootEmUp
     [RequireComponent(typeof(PlayerInput))]
     public class CharacterMovement : MonoBehaviour
     {
-        public Vector2 movementInput = default;
+        [NonSerialized] public Vector2 movementInput = default;
         [SerializeField] private float speed = 10f;
-            
+        private Transform playerTransform;
+
+        private void Awake()
+        {
+            playerTransform = transform;
+        }
+
         private void Update()
         {
-            transform.position += (Vector3) movementInput * (speed * Time.deltaTime);
+            playerTransform.position += (Vector3) movementInput * (speed * Time.deltaTime);
         }
     }
 }
