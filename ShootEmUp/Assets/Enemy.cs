4e8d52dfed04b37d55cd92cc1fb7d35aed1ec654
Teodor Tysklind <37851342+TeoC00l@users.noreply.github.com>
2020-09-17 18:18:50 +0200

    sin()

---
+++ ShootEmUp/Assets/Enemy.cs
@@ -0,0 +1,16 @@
+﻿//@Author: Teodor Tysklind / FutureGames / Teodor.Tysklind@FutureGames.nu
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Vector3 moveDirection = Vector3.down + Vector3.right;
+    private float amplitude = 2f;
+    private float frequency = 2f;
+    private void Update()
+    {
+        Vector3 _newPosition = transform.position;
+        _newPosition.y += Mathf.Sin(Time.time * frequency) * amplitude * Time.deltaTime;
+        transform.position = _newPosition + moveDirection * Time.deltaTime;
+    }
+}
